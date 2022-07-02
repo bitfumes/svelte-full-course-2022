8fb2e0108dfaae43fad574cfbd605761278e883d
sarthaksavvy <sarthak@bitfumes.com>
2022-07-02 19:54:48 +0530

    06 Svelte Course - loading assets

--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -1,16 +1,42 @@
 <script>
-  let user = {
-    image: "https://cdn-icons-png.flaticon.com/128/3135/3135715.png",
-    name: "John",
-    email: "John@bitfumes.com",
-  };
+  import user1 from "../assets/images/user1.png";
+  import user2 from "../assets/images/user2.png";
+  import user3 from "../assets/images/user3.png";
+
+  let users = [
+    {
+      image: user1,
+      name: "John",
+      email: "John@bitfumes.com",
+    },
+    {
+      image: user2,
+      name: "Jeni",
+      email: "Jenie@bitfumes.com",
+    },
+    {
+      image: user3,
+      name: "Hacker",
+      email: "hacker@bitfumes.com",
+    },
+  ];
 </script>
 
 <div>
   <h1>List of Users</h1>
   <div>
-    <img src={user.image} alt={user.name} />
-    <p>{user.name}</p>
-    <p>{user.email}</p>
+    <img src={users[0].image} alt={users[0].name} />
+    <p>{users[0].name}</p>
+    <p>{users[0].email}</p>
+  </div>
+  <div>
+    <img src={users[1].image} alt={users[1].name} />
+    <p>{users[1].name}</p>
+    <p>{users[1].email}</p>
+  </div>
+  <div>
+    <img src={users[2].image} alt={users[2].name} />
+    <p>{users[2].name}</p>
+    <p>{users[2].email}</p>
   </div>
 </div>
