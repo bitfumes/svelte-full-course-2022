0d353cf1399751567404aad8b21348b61d0190b8
sarthaksavvy <sarthak@bitfumes.com>
2022-07-02 19:38:27 +0530

    05 Svelte Course - create first component

---
+++ src/lib/Users.svelte
@@ -0,0 +1,16 @@
+<script>
+  let user = {
+    image: "https://cdn-icons-png.flaticon.com/128/3135/3135715.png",
+    name: "John",
+    email: "John@bitfumes.com",
+  };
+</script>
+
+<div>
+  <h1>List of Users</h1>
+  <div>
+    <img src={user.image} alt={user.name} />
+    <p>{user.name}</p>
+    <p>{user.email}</p>
+  </div>
+</div>
