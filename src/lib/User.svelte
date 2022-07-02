d427fae8c39b6531b73c953923056c63352e7718
sarthaksavvy <sarthak@bitfumes.com>
2022-07-02 20:41:19 +0530

    09 Svelte Course - component props

---
+++ src/lib/User.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let user;
+  export let i;
+</script>
+
+<div
+  class="flex m-5 bg-white border p-2 rounded-lg shadow-md hover:shadow-lg cursor-pointer items-center"
+>
+  <p class="w-10">{i + 1}</p>
+  <img class="rounded-full w-14 h-14" src={user.image} alt={user.name} />
+  <p class="mx-4 w-20">{user.name}</p>
+  <p class="mx-4 w-20">{user.email}</p>
+</div>
--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -2,6 +2,7 @@
   import user1 from "../assets/images/user1.png";
   import user2 from "../assets/images/user2.png";
   import user3 from "../assets/images/user3.png";
+  import User from "./User.svelte";
 
   let users = [
     {
@@ -9,18 +10,21 @@
       image: user1,
       name: "John",
       email: "John@bitfumes.com",
+      active: false,
     },
     {
       id: 2,
       image: user2,
       name: "Jeni",
       email: "Jenie@bitfumes.com",
+      active: true,
     },
     {
       id: 3,
       image: user3,
       name: "Hacker",
       email: "hacker@bitfumes.com",
+      active: false,
     },
   ];
 
@@ -31,15 +35,8 @@
 
 <div>
   <h1 class="text-2xl text-center mt-10">List of Users</h1>
-  {#each getUsers() as { image, name, email, id }, i (id)}
-    <div
-      class="flex m-5 bg-white border p-2 rounded-lg shadow-md hover:shadow-lg cursor-pointer items-center"
-    >
-      <p class="w-10">{i + 1}</p>
-      <img class="rounded-full w-14 h-14" src={image} alt={name} />
-      <p class="mx-4 w-20">{name}</p>
-      <p class="mx-4 w-20">{email}</p>
-    </div>
+  {#each getUsers() as user, i (user.id)}
+    <User {user} {i} />
   {:else}
     <p>No user found</p>
   {/each}
